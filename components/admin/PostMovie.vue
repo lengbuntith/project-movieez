f956c833cdba052b530ee7c6c22ac52ff415fcec
Bongkruysk <limkimnguon2020@gmail.com>
2021-08-14 16:24:02 +0700

    I changed title at nav

--- components/admin/PostMovie.vue
+++ components/admin/PostMovie.vue
@@ -10,7 +10,7 @@
 
       <v-card>
         <v-card-title class="text-h5 grey lighten-2">
-          Add new movie
+          Submit
           <v-spacer />
           <v-btn @click="dialog = false" icon>
             <v-icon>mdi-close</v-icon>
