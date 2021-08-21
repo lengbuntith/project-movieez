bf3a80c208b61cc8d44b8433783111d36f2b77f0
Bongkruysk <limkimnguon2020@gmail.com>
2021-08-21 20:27:59 +0700

    change submit

--- components/admin/PostMovie.vue
+++ components/admin/PostMovie.vue
@@ -10,7 +10,7 @@
 
       <v-card>
         <v-card-title class="text-h5 grey lighten-2">
-          Submit Change
+          Submit changed
           <v-spacer />
           <v-btn @click="dialog = false" icon>
             <v-icon>mdi-close</v-icon>
