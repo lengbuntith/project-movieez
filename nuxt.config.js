c2f6fed4afa8d5456b3843d66de972f4584264f5
leng <lengbuntith2000@gmail.com>
2021-09-06 18:52:14 +0700

    change to full static site

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,8 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  ssr: false,
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - movieez',
