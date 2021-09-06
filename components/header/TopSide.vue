dac38583b6ad3da7fefc59a73abfd97640a41f03
leng <lengbuntith2000@gmail.com>
2021-09-06 18:44:32 +0700

    add function search and switch mode light/dark :

--- components/header/TopSide.vue
+++ components/header/TopSide.vue
@@ -1,23 +1,52 @@
 <template>
   <v-app-bar app>
-      <v-row class=" no-gutters">
-          <v-col>
-              <v-text-field  rounded hide-details outlined placeholder="search..."></v-text-field>
-          </v-col>
+    <div class="ml-7">
+      <v-text-field
+        dense
+        rounded
+        hide-details
+        outlined
+        placeholder="search..."
+        v-model="search"
+        @keypress.enter="searchMovie"
+        prepend-inner-icon="mdi-magnify"
+      ></v-text-field>
+    </div>
 
-          <v-col>
+    <v-spacer></v-spacer>
 
-          </v-col>
-
-          <v-col>
-
-          </v-col>
-      </v-row>
+    <div>
+      <v-switch
+        v-model="switch1"
+        hide-details
+        inset
+        @change="toggleDarkMode"
+        :label="`Switch 1: ${switch1.toString()}`"
+      ></v-switch>
+    </div>
   </v-app-bar>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      switch1: false,
+      search: '',
+    }
+  },
+
+  methods: {
+    toggleDarkMode: function () {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+    },
+
+    searchMovie() {
+      console.log(this.search)
+      this.$router.push('/search/' + this.search)
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped>
