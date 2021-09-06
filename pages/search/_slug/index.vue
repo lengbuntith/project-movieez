94a48407f989d738eb15a81c3902c4ddba95eb54
leng <lengbuntith2000@gmail.com>
2021-09-06 18:45:15 +0700

    add more pages

---
+++ pages/search/_slug/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{ $route.params.slug }}
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.getMovie()
+  },
+
+  methods: {
+    async getMovie() {
+      let textSearch = this.$route.params.slug
+      const res = await this.$supabase
+        .from('movies')
+        .select('*')
+        .like('name', textSearch)
+      console.log('search result', res)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
