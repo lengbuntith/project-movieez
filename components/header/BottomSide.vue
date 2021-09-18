4045b018527e784690152360d310902e2d848a88
leng <lengbuntith2000@gmail.com>
2021-09-18 18:24:36 +0700

    add bottom navigation

--- components/header/BottomSide.vue
+++ components/header/BottomSide.vue
@@ -1,34 +1,35 @@
 <template>
-  <v-bottom-navigation v-if="show" class="d-md-none" :value="value" color="teal" grow>
-    <v-btn v-for="(item, index) in items" :key="index" :to="item.link">
-      <span>{{ item.title }}</span>
-
-      <v-icon>{{ item.icon }}</v-icon>
-    </v-btn>
-  </v-bottom-navigation>
+  <div id="bottom-nav">
+    <v-bottom-navigation class="d-md-none" color="teal" grow>
+      <v-btn 
+      v-for="(item, index) in items" 
+      :key="index" 
+      :to="item.link">
+        <span>{{ item.title }}</span>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    value: 1,
-    show: true,
     items: [
       { title: 'Home', icon: 'mdi-home', link: '/' },
-      { title: 'Genres', icon: 'mdi-view-dashboard', link: '/genre' },
+      { title: 'Genres', icon: 'mdi-filter', link: '/genre' },
       { title: 'Favorite', icon: 'mdi-heart', link: '/favorite' },
       { title: 'Account', icon: 'mdi-account', link: '/account' },
     ],
   }),
-
-  created () {
-      this.$nuxt.$on('close-left-side', () => {
-        this.show = false
-    })
-  },
-
-  beforeDestroy () {
-      this.$nuxt.$off('close-left-side')
-  },
 }
 </script>
+
+<style lang="scss" scoped>
+#bottom-nav {
+  position: fixed;
+  bottom: 0;
+  z-index: 999;
+  width: 100%;
+}
+</style>
